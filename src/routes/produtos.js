b75423020a937fa526120adffd4020d860b326c3
unknown <andrelimanegocio@gmail.com>
2025-06-09 12:00:42 -0300

    back-seg

--- src/routes/produtos.js
+++ src/routes/produtos.js
@@ -5,7 +5,7 @@ const produtosMiddleware = require('../middlewares/produtos')
 const authMiddleware = require('../middlewares/auth')
 
 router.get('/produtos', authMiddleware.validateToken,produtosController.getProdutos)
-router.post('/produtos', produtosMiddleware.validateCreateProduto, produtosController.createProduto)
-router.delete('/produtos/:id', produtosMiddleware.validateDeleteProduto, produtosController.deleteProduto)
+router.post('/produtos',authMiddleware.validateToken, produtosMiddleware.validateCreateProduto, produtosController.createProduto)
+router.delete('/produtos/:id',authMiddleware.validateToken, produtosMiddleware.validateDeleteProduto, produtosController.deleteProduto)
 
 module.exports = router;
